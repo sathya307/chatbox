9d48ae1423e5dbe646bc0846988daad46dd1a1ab
sathya <59756507+sathya307@users.noreply.github.com>
2020-01-24 11:52:25 +0530

    Add files via upload
    CHAT BOX
    
    A simple chating program. user in same connections can chat with eachother. 
    i used websockets, and mainly through  django channels.
    project name is locker. app name is chat.
    through a login system it is secured, only authorised user can get into the room and begin the chat.
    i used redis, Redis is an in-memory data structure project implementing a distributed, in-memory key-value database with optional durability. Redis supports different kinds of abstract data structures, such as strings, lists, maps, sets, sorted sets, HyperLogLogs, bitmaps, streams, and spatial indexes.
    you need to install redis for this project to work.
    use this viedo for installation "https://www.youtube.com/watch?v=188Fy-oCw4w"

---
+++ chat/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+from django.contrib.auth.decorators import login_required
+import json
+
+def index(request):
+    return render(request, 'chat/index.html', {})
+
+@login_required()
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username)),
+    })
+
+# Create your views here.
